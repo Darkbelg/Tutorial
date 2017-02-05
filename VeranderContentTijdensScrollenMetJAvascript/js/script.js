77e4b654d1dbd437580c57317f6022dc4a39ab3b
Darkbelg <stijn.sagaert@outlook.com>
2017-02-05 21:09:06 +0100

    met Verander content tijdens scrollen met javascript

---
+++ VeranderContentTijdensScrollenMetJAvascript/js/script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    console.log("Hello world");
+    var waypoint = new Waypoint({
+        element:document.getElementById('block'),
+        handler:function(direction){
+            this.element.classList.remove('original');
+            this.element.classList.add('change');
+        },
+        offset:'-5%'
+    })
+};
