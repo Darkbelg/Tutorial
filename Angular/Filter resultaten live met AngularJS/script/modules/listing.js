ed11f598a61e31b60ebdac59925437d2d95ece22
Darkbelg <stijn.sagaert@outlook.com>
2017-03-19 17:44:02 +0100

    met angular basis tutorial

--- Angular/Filter resultaten live met AngularJS/script/modules/listing.js
+++ Angular/Filter resultaten live met AngularJS/script/modules/listing.js
@@ -1,7 +1,7 @@
 /**
  * Created by stijn on 01-Mar-17.
  */
-angular.module('listing.module', ['listing.services']).controller('listingCtrl', ['$scope', 'data',
+angular.module('listing.module', ['listing.services','listing.filters']).controller('listingCtrl', ['$scope', 'data',
     function ($scope, data) {
         "use strict";
         $scope.title = "Technology News";
@@ -11,4 +11,31 @@ angular.module('listing.module', ['listing.services']).controller('listingCtrl',
         }
         data.get('article.json', $scope.setData);
         $scope.viewLimit = 1;
+        $scope.viewMore = function (num) {
+            $scope.viewLimit += num;
+        }
+        //default descending
+        $scope.descending=true;
+
+        $scope.filters = {
+            qeury:"",
+            tags:[
+                {
+                    label:"tag1",
+                    selected:false
+                },
+                {
+                    label:"tag2",
+                    selected:false
+                },
+                {
+                    label:"tag3",
+                    selected:false
+                },
+                {
+                    label:"tag4",
+                    selected:false
+                }
+            ]
+        }
     }]);
