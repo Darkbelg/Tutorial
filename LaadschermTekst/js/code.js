2714496bfaff1844470dc330a2d68a14675efd83
Darkbelg <stijn.sagaert@outlook.com>
2017-08-08 20:49:20 +0200

    With stencil loader

---
+++ LaadschermTekst/js/code.js
@@ -0,0 +1,6 @@
+window.addEventListener("load",function () {
+    document.body.className+="loaded";
+    // setTimeout(function () {
+    //     document.body.className+=" loaded ";
+    // },5000);
+});
